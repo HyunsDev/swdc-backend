dce07eecf1441c8baec6786954fd64e99c06998b
혀느현스 <hyuns@hyuns.dev>
2022-07-31 17:23:08 +0900

    주석 추가

--- app.js
+++ app.js
@@ -1,30 +1,71 @@
+
+// 라이브러리 불러오기
 const express = require('express')
 const createError = require('http-errors')
 const logger = require('morgan')
 const cors = require('cors')
 const path = require('path')
+
+// .env 파일 불러오기
+// 불러온 .env 파일은 process.env에 자동으로 저장된다.
+// 사용법 'process.env.키'
+// .env 파일은 비밀번호 등 중요한 정보가 들어있기 때문에 git에 업로드 하지 않고 내 컴퓨터에만 보관한다.
+// .env.example 를 복붙해서 .env 바꿔준 다음 내용을 채워보자
 require('dotenv').config()
 
+// 라우터 불러오기
 const boardRouter = require('./router/board')
+
+
+// 익스프레이스 앱 객체
 const app = express()
 
+
+/*
+[ 미들웨어 ]
+Express에서 각 요청은 아래 미들웨어를 한 번씩 거치게 됩니다.
+*/
+
+// CORS 방지: (Search: CORS)
 app.use(cors())
+
+// 콘솔에 접속 로그 표시 (Search: morgan)
+// 'dev': 로그 레벨 ()
 app.use(logger('dev'))
+
+// Body 데이터를 string에서 json으로 변환
 app.use(express.json())
+
+// URL encode된 데이터를 원래 데이터로 변환
 app.use(express.urlencoded({ extended: false }));
+
+// public 폴더를 정적 폴더로 지정
+// 정적 폴더: 폴더 안에 있는 파일을 그대로 전송
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// 아까전에 불러온 Board 라우터를 /board 에 등록
+// 라우터도 미들웨어의 일종
+// '/board'로 들어온 요청은 boardRouter로 전송됨 
 app.use('/board', boardRouter)
 
+
+// 위에서 요청을 처리하지 못한 경우 === 파일이 없는 경우 => 404 오류
+// 404오류란?: Search HTTP 응답 코드
 app.use((req, res, next) => {
     next(createError(404))
 })
 
+
+// Express에서 발생한 오류를 처리 (메게변수가 3개 => 일반 미들웨어, 4개 => 오류 처리 미들웨어)
 app.use((err, req, res, next) => {
     res.status(err.status)
     res.send(err)
 })
 
+// 서버를 최종적으로 실행, PORT 3000번
+// 서버 실행 후 http://localhost:3000 에서 접속 가능
+// 포트 번호 뒤에 함수는 서버가 시작됬을 때 실행하는 함수
 app.listen(3000, () => {
     console.log('Server Started!')
 })
--- router/board.js
+++ router/board.js
@@ -1,8 +1,15 @@
+// 라이브러리 불러오기
 const express = require('express')
 const mysql = require('mysql2/promise')
 
+// express 라우터
 const router = express.Router()
 
+// 데이터베이스 커넥션풀을 생성
+// 커넥션풀: 커넥션을 위한 정보 저장
+// 커넥션(connection): 데이터베이스와의 연결
+// 커넥션을 하는 이유: 데이터베이스를 사용(읽기, 쓰기, 수정, 삭제 등)하기 위함
+// 'process.env'?: 아까 .env 파일 불러온 결과
 const connectPool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -12,57 +19,129 @@ const connectPool = mysql.createPool({
 
 
 // 전체 글 목록
+// board 라우터가 '/board'에 등록되었기 때문에, '/board/'(또는 '/board') 로 접근해야 됨
 router.get('/', async (req, res) => {
+    // DB 커넥션 생성
     const conn = await connectPool.getConnection()
+
+    // DB에서 board 테이블 조회
+    // SQL: "SELECT (칼럼1, 칼럼2) FROM '테이블' 로 작성하여 읽기 가능"
     const result = await conn.query('SELECT * FROM `board`')
+
+    // 만약 DB 결과를 직접 보고 싶다면 아래 코드를 주석 해체하고 실행
+    //console.log(result[0])
+
+
+    // DB 작업이 끝나면 커넥션을 해제해야 함.
+    // 해제를 하지 않고 계속 사용한 경우 최대 커넥션 제한에 걸림
     conn.release()
+
+    // DB 결과를 클라이언트에게 전송.
+    // res.send() 등으로 클라이언트에게 응답을 한 경우 다음 미들웨어를 처리하지 않음
     res.send(result[0])
 })
 
 // 특정 아이디 글 정보
+// ':id'의 의미: /board/1 이렇게 접근했을 때 '1'이 자동으로 req.params.id로 저장됨
 router.get('/:id', async (req, res) => {
+    // :id로 받은 글 아이디를 'id'변수에 저장
     const id = req.params.id
+
+    // DB 커넥션 생성
     const conn = await connectPool.getConnection()
+
+    // DB에서 특정 row 조회
+    // 일반적인 조회 명령어 뒤에 "WHERE 조건"을 추가해서 해당 조건에 맞는 row만 조회 가능
+    // "?"와 뒤의 배열의 의미: 만약 ? 자리에 변수를 그대로 넣으면 SQL Injection이라는 해킹을 받을 수 있음
+    // 클라이언트에서 받는 신뢰할 수 없는 정보는 무조건 ?로 채우고 뒤에 리스트로 담기
+    // *혹시 진로가 보안 쪽이라면 SQL Injection에 대해 더 알아보면 좋습니다. 기본적이면서 굉장히 치명적인 해킹 기법이에요.
     const result = await conn.query('SELECT * FROM `board` WHERE id = ?', [ id ])
+
+    // DB 커넥션 해제
     conn.release()
+
+    // DB 결과를 클라이언트에게 응답
     res.send(result[0])
 })
 
 // 글 작성
+// HTTP Method가 post이면서 '/board'로 요청한 요청을 처리
+// HTTP Method란? Search HTTP Method
 router.post('/', async (req, res) => {
+    // body로 title과 content 받기
+    // 아래와 같이 변수를 저장하는 방법 -> Search 구조 분해 할당
     const { title, content } = req.body
+
+    // 생성 시간 = 현재 시간
     const createdAt = new Date()
+
+    // 수정 시간 = 현재 시간
     const updatedAt = new Date()
 
+    // 만약 title이나 content가 없다면
     if (!title || !content) {
+
+        // 400(Bad Request) 응답 코드와 함께 클라이언트에게 응답
         res.status(400).send({
             message: "정보가 부족합니다."
         })
+        return
     }
 
+    // 커넥션 생성
     const conn = await connectPool.getConnection()
+
+    // 새로운 글 생성
     const result = await conn.query('INSERT INTO `board` (title, content, createdAt, updatedAt) value (?, ?, ?, ?)', [ title, content, createdAt, updatedAt ])
     
+    // DB 커넥션 해제
     conn.release()
+
+    // 새로운 글의 아이디 전송
     res.send({id: result[0].insertId})
 })
 
 // 글 삭제
+// HTTP Method가 delete이면서 '/:id'로 요청한 요청을 처리
 router.delete('/:id', async (req, res) => {
+    // DB 커넥션 생성
     const conn = await connectPool.getConnection()
+
+    // 글 삭제
+    // 만약 해당하는 글이 없더라도 오류가 발생하지 않음.
+    // 만약 삭제가 된건지 무시된건지 알고 싶다면 글 생성 코드처럼 result로 쿼리 결과를 저장한 다음 결과를 분석해보면 됨. ( 직접 해보세요 :) )
+    // - 또는 먼저 해당 글을 조회한 다음 글이 있는지 보고 아래 코드를 처리해도 됨. 방법은 진짜 많음
     await conn.query('DELETE FROM `board` WHERE id = ?', [ req.params.id ])
+
+    // DB 커넥션 해체
     conn.release()
+
+    // 빈 응답 전송: 딱히 전송할 데이터가 없으면 그냥 빈 응답 보내도 됨.
     res.send()
 })
 
 // 글 수정
+// HTTP Method가 patch '/:id'로 요청한 요청을 처리
 router.patch('/:id', async (req,res) => {
+    // :id로 받은 글 아이디를 'id'변수에 저장
     const id = req.params.id
+
+    // body에서 title과 content
     const { title, content } = req.body
+
+    // 수정한 시간 = 현재 시간
     const updatedAt = new Date()
+
+    // 데이터베이스 커넥션 생성
     const conn = await connectPool.getConnection()
+
+    // 해당 글 수정
     await conn.query('UPDATE `board` SET title = ?, content = ?, updatedAt = ? WHERE id = ?', [ title, content, updatedAt, id ])
+    
+    // 커넥션 해제
     conn.release()
+
+    // 빈 응답 전송
     res.send()
 })
 
